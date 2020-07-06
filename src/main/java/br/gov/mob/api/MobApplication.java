ea48ab8972478efbc134443d27dcc8be0f4f7ce9
Eduardo Farias <eduardofarias@pop-os.localdomain>
2020-07-06 15:55:52 -0300

    init project commit

---
+++ src/main/java/br/gov/mob/api/MobApplication.java
@@ -0,0 +1,12 @@
+package br.gov.mob.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobApplication.class, args);
+    }
+}
